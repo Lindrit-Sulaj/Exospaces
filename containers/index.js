94a4f59273dbd84de958a87ed18a70426df069e9
Lindrit <lindritsulaj2.0@gmail.com>
2023-05-24 21:24:26 +0200

    Sections Added

---
+++ containers/index.js
@@ -0,0 +1,15 @@
+import Hero from "./Hero";
+import About from "./About";
+import Projects from "./Projects";
+import Benefits from "./Benefits";
+import Products from "./Products";
+import Testimonials from "./Testimonials";
+
+export {
+  Hero,
+  About,
+  Projects,
+  Benefits,
+  Products,
+  Testimonials
+}
